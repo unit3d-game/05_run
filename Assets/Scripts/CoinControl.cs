9cbeba702608d84b582334297a251b49c05c1c24
shifenghu <chinashifenghu@gmail.com>
2022-11-04 10:03:04 +0800

    金币逻辑
    添加了金币逻辑

--- Assets/Scripts/CoinControl.cs
+++ Assets/Scripts/CoinControl.cs
@@ -21,39 +21,68 @@ public class CoinControl : MonoBehaviour
     public void CreateGroup(GameObject ground)
     {
         // 随机coin 类型
-        //int random = UnityEngine.Random.Range(0, 100);
-        //foreach (CoinGroup cg in groups)
-        //{
-        //    if (cg.probability > random)
-        //    {
-        //        cg.action(ground);
-        //        break;
-        //    }
-        //}
-        CreateOne(ground);
+        int random = UnityEngine.Random.Range(0, 100);
+        foreach (CoinGroup cg in groups)
+        {
+            if (cg.probability > random)
+            {
+                cg.action(ground);
+                break;
+            }
+        }
     }
 
 
     private void CreateOne(GameObject ground)
     {
-        GameObject coinPrefab = CoinPrefabs[UnityEngine.Random.Range(0, CoinPrefabs.Length)];
-        GameObject coin = Instantiate(coinPrefab, ground.transform);
-        Vector3 pos = coin.transform.position;
         GroundObject goo = ground.GetComponent<GroundObject>();
-        pos.y = UnityEngine.Random.Range(0f, 2f);
-        pos.x = UnityEngine.Random.Range(0f, goo.renderWidth);
-        coin.transform.position = pos;
+        Vector2 vector2 = new Vector2(UnityEngine.Random.Range(0f, goo.RenderWidth), UnityEngine.Random.Range(0f, 0.8f));
+        CreateACoin(CoinPrefabs[UnityEngine.Random.Range(0, CoinPrefabs.Length)], ground, vector2);
     }
 
     private void CreateVGroup(GameObject ground)
     {
-
-
+        GroundObject goo = ground.GetComponent<GroundObject>();
+        Vector2 vector2 = new Vector2(UnityEngine.Random.Range(0f, 1f), 0f);
+        CreateACoin(CoinPrefabs[0], ground, new Vector2(vector2.x - 0.6f, vector2.y + 0.8f));
+        CreateACoin(CoinPrefabs[0], ground, new Vector2(vector2.x - 0.3f, vector2.y + 0.4f));
+        CreateACoin(CoinPrefabs[0], ground, vector2);
+        CreateACoin(CoinPrefabs[0], ground, new Vector2(vector2.x + 0.3f, vector2.y + 0.4f));
+        CreateACoin(CoinPrefabs[0], ground, new Vector2(vector2.x + 0.6f, vector2.y + 0.8f));
     }
 
-    private void CreateNGroup(GameObject ground) { }
+    private void CreateACoin(GameObject coinPrefab, GameObject ground, Vector2 position)
+    {
+        GameObject coin = Instantiate(coinPrefab, ground.transform);
+        coin.transform.localScale = Vector3.one;
+        Vector3 pos = coin.transform.position;
+        pos.y = position.y;
+        pos.x = position.x;
+        coin.transform.position = pos;
+    }
 
-    private void CreateLineGroup(GameObject ground) { }
+
+    private void CreateNGroup(GameObject ground)
+    {
+        GroundObject goo = ground.GetComponent<GroundObject>();
+        Vector2 vector2 = new Vector2(UnityEngine.Random.Range(0f, 1f), 0.8f);
+        CreateACoin(CoinPrefabs[0], ground, new Vector2(vector2.x - 0.6f, vector2.y - 0.8f));
+        CreateACoin(CoinPrefabs[0], ground, new Vector2(vector2.x - 0.3f, vector2.y - 0.4f));
+        CreateACoin(CoinPrefabs[1], ground, vector2);
+        CreateACoin(CoinPrefabs[0], ground, new Vector2(vector2.x + 0.3f, vector2.y - 0.4f));
+        CreateACoin(CoinPrefabs[0], ground, new Vector2(vector2.x + 0.6f, vector2.y - 0.8f));
+    }
+
+    private void CreateLineGroup(GameObject ground)
+    {
+        GroundObject goo = ground.GetComponent<GroundObject>();
+        Vector2 vector2 = new Vector2(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f));
+        int size = UnityEngine.Random.Range(1, 5);
+        for (var i = 0; i < size; i++)
+        {
+            CreateACoin(CoinPrefabs[0], ground, new Vector2(vector2.x + i * 0.3f, vector2.y));
+        }
+    }
 }
 
 public class CoinGroup
